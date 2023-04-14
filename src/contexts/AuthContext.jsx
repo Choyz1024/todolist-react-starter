d2159722df4e95492a93f839a56898e06a2ce98c
choyz <hc.wang@wingene.com.tw>
2023-04-14 16:15:47 +0800

    feat: add auth provider

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,6 +1,7 @@
-import { login, register } from 'api/auth';
+import { checkPermission, login, register } from 'api/auth';
 import * as jwt from 'jsonwebtoken';
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
 const defaultAuthContext = {
   isAuthenticated: false, // 使用者是否登入的判斷依據，預設為 false，若取得後端的有效憑證，則切換為 true
@@ -11,9 +12,34 @@ const defaultAuthContext = {
 };
 
 const AuthContext = createContext(defaultAuthContext);
+
 export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [payload, setPayload] = useState(null);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    const checkTokenIsValid = async () => {
+      const authToken = localStorage.getItem('authToken');
+      if (!authToken) {
+        setIsAuthenticated(false);
+        setPayload(null);
+        return;
+      }
+      const result = await checkPermission(authToken);
+      if (result) {
+        setIsAuthenticated(true);
+        const tempPayload = jwt.decode(authToken);
+        setPayload(tempPayload);
+      } else {
+        setIsAuthenticated(false);
+        setPayload(null);
+      }
+    };
+
+    checkTokenIsValid();
+  }, [pathname]);
+
   return (
     <AuthContext.Provider
       value={{
